7b640214f8ba1d81c2c002ae3b41e005d4716fea
DoanNgocTrong <trong.doanngoc2023@gmail.com>
2024-11-12 11:09:10 +0700

    init: portfolio

---
+++ src/data/resume.tsx
@@ -0,0 +1,287 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Trong Doan Ngoc",
+  initials: "DNT",
+  url: "https://dillion.io",
+  location: "San Francisco, CA",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  description:
+    "To apply my expertise in front-end development with a focus on ReactJS and React Native to drive innovation and growth in a dynamic organization. I aim to create responsive, scalable applications and collaborate effectively within cross-functional teams, continuously advancing my skills to deliver exceptional, user-centered products.",
+  summary:
+    "Front-end developer with 2 years of experience in web (ReactJS) and mobile (React Native) applications for clients across Education, E-commerce, Blockchain, and Social Networking. Skilled in creating accessible, user-friendly interfaces and committed to delivering excellent user experiences.",
+  avatarUrl:
+    "https://scontent.fsgn5-8.fna.fbcdn.net/v/t39.30808-6/452206383_3489505904674587_5235226131960106067_n.jpg?stp=cp6_dst-jpg_s206x206&_nc_cat=109&ccb=1-7&_nc_sid=fe5ecc&_nc_ohc=bK544tYNGn8Q7kNvgE2A5_9&_nc_zt=23&_nc_ht=scontent.fsgn5-8.fna&_nc_gid=A6yTaEdh6XQ0epYedN4ySg7&oh=00_AYBBjop3CvYCqmfnUOcGjqQOrZxeHCwAcwpU8s_blN1DHw&oe=67387E7E",
+  skills: ["React.js", "React Native", "Next.js", "Typescript", "Node.js"],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    // { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "trong.doanngoc2023@gmail.com",
+    tel: "+84335764560",
+    social: {
+      Email: {
+        name: "Send Email",
+        url: "mailto:trong.doanngoc2023@gmail.com",
+        icon: Icons.email,
+        navbar: true,
+      },
+      Phone: {
+        name: "Call Me",
+        url: "tel:+84335764560",
+        icon: Icons.phone,
+        navbar: true,
+      },
+      Facebook: {
+        name: "Facebook",
+        url: "https://www.facebook.com/doanngoctrong2001",
+        icon: Icons.contact,
+        navbar: true,
+      },
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/uSerDnt",
+        icon: Icons.github,
+        navbar: true,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "BITECH DEVELOPMENT INVESTMENT CO.,LTD",
+      badges: [],
+      href: "https://store.bitechco.com/",
+      location: "43 Đường Số 37, Khu đô Thị Vạn Phúc, Thủ Đức, Hồ Chí Minh",
+      title: "Frontend Developer",
+      logoUrl: "/bitech.png",
+      start: "Sep 2023",
+      end: "Nov 2024",
+      description:
+        "Specializes in the production and implementation of information technology products applied in the fields of Education & Healthcare.",
+    },
+    {
+      company: "NAMI CORP",
+      badges: [],
+      href: "https://nami.exchange/",
+      location: "Remote",
+      title: "Frontend Developer",
+      logoUrl: "/nami.png",
+      start: "Oct 2022",
+      end: "Feb 2023",
+      description:
+        "Nami exchange is an online exchange where users can trade crypto currencies.",
+    },
+  ],
+  education: [
+    {
+      school: "SaiGon Technology University (STU)",
+      href: "https://www.stu.edu.vn/",
+      degree: "Bachelor's degree of Information Technology",
+      logoUrl: "/stu.jpg",
+      start: "2019",
+      end: "2023",
+    },
+    {
+      school: "English Center",
+      href: "",
+      degree: "~ 450",
+      logoUrl: "/toeic.jpg",
+      start: "2019",
+      end: "2023",
+    },
+  ],
+  projects: [
+    {
+      title: "BiEdu (Web - App)",
+      href: "https://bi-edu.bitechco.com/",
+      dates: "Sep 2023 - Present",
+      active: true,
+      description:
+        "BiEdu Software is an educational platform that combines web and mobile applications, aimed at facilitating school management, teacher coordination, and seamless information exchange among students, teachers, and parents.",
+      technologies: [
+        "React Native",
+        "React.js",
+        "Javascript",
+        ".Net",
+        "SignalR",
+        "MongoDB",
+        "RabbitMQ",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://bi-edu.bitechco.com/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Appstore",
+          href: "/",
+          icon: <Icons.appstore className="size-3" />,
+        },
+        {
+          type: "CH Play",
+          href: "/",
+          icon: <Icons.chplay className="size-3" />,
+        },
+      ],
+      image: "/biedu.jpg",
+      video: "",
+    },
+    {
+      title: "Bikids (Web - App)",
+      href: "https://bikids.edu.vn/",
+      dates: "Sep 2023 - Present",
+      active: true,
+      description:
+        "An educational application designed to help schools manage student records, teacher information, tuition fee payments, child nutrition, and academic results.",
+      technologies: [
+        "React Native",
+        "React.js",
+        "Javascript",
+        ".Net",
+        "SignalR",
+        "MongoDB",
+        "RabbitMQ",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://bikids.edu.vn/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Appstore",
+          href: "/",
+          icon: <Icons.appstore className="size-3" />,
+        },
+        {
+          type: "CH Play",
+          href: "/",
+          icon: <Icons.chplay className="size-3" />,
+        },
+      ],
+      image: "/bikid.png",
+      video: "",
+    },
+    {
+      title: "Nami Exchange (App)",
+      href: "https://nami.exchange/",
+      dates: "Oct 2022 - Feb 2023",
+      active: true,
+      description:
+        "Nami exchange is an online exchange where users can trade crypto currencies.",
+      technologies: [
+        "React Native",
+        "Next.js",
+        "Typescript",
+        "Node.js",
+        "MySQL",
+        "MongoDB",
+        "Socket.io",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://nami.exchange/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Appstore",
+          href: "/",
+          icon: <Icons.appstore className="size-3" />,
+        },
+        {
+          type: "CH Play",
+          href: "/",
+          icon: <Icons.chplay className="size-3" />,
+        },
+      ],
+      image: "/nami-web.png",
+      video: "",
+    },
+    {
+      title: "StyleBorrow",
+      href: "https://dntbooking.vercel.app/",
+      dates: "April 2024 - June 2024",
+      active: true,
+      description:
+        "StyleBorrow is a webapp and mobile application project aimed at facilitating both clothing rental and sales services",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Firebase",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://dntbooking.vercel.app/",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/styleborrow.png",
+      video: "",
+    },
+    {
+      title: "Jirra-clone",
+      href: "https://planningdev.vercel.app/",
+      dates: "Nov 2024 - Present",
+      active: true,
+      description:
+        "Developed a clone of Jira, a web application for project management and bug tracking.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "Appwrite",
+        "Hono.js",
+        "TailwindCSS",
+        "Shadcn UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://planningdev.vercel.app/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/uSerDnt/PlanningWeb",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/jirra.png",
+      video: "",
+    },
+    {
+      title: "You want to see more of my projects?",
+      href: "https://github.com/uSerDnt",
+      dates: "Forever",
+      active: true,
+      description: "",
+      technologies: [
+        "React Native",
+        "Next.js",
+        "Typescript",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Firebase",
+        "Magic UI",
+        "Ant Design",
+      ],
+      links: [
+        {
+          type: "Source",
+          href: "https://github.com/uSerDnt",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/contri.png",
+      video: "",
+    },
+  ],
+};
